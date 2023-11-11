fecca32f536774eba1894013b471f0f87f8fa9e1
Cody <codylau6233@hotmail.com>
2023-11-10 20:24:41 -0800

    Add manual test for fetchISSFlyOverTimes function in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const {fetchCoordsByIP} = require('./iss');
+const {fetchISSFlyOverTimes} = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,12 +10,23 @@ const {fetchCoordsByIP} = require('./iss');
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
-let ip = '70.66.152.215';
-fetchCoordsByIP(ip, (error, data) => {
+
+// let ip = '70.66.152.215';
+// fetchCoordsByIP(ip, (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned lat/lng:' , data);
+// });
+
+fetchISSFlyOverTimes({latitude: 70, longitude: 100}, (error, data) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned lat/lng:' , data);
+  console.log("It worked! Returned Fly Over Times: ", data);
+
 });
