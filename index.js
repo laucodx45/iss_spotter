3e9014ca4c8ee8c60e45ff4cfe4c3cf22d0dbf20
Cody <codylau6233@hotmail.com>
2023-11-10 19:02:00 -0800

    Test fetchCoordsByIP function in index.js, function is working as expected

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const { fetchMyIP } = require('./iss');
+const {fetchCoordsByIP} = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// }); '70.66.152.215'
+
+fetchCoordsByIP('42', (error, data) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned lat/lng:' , data);
 });
