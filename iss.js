5cf9a290f6c5b705caa4a0e4be218ebc625185b4
Cody <codylau6233@hotmail.com>
2023-11-10 16:27:12 -0800

    Refactor iss.js: -Refactori fetchMyIP function - function now parse and extract IP address using JSON -function now logs statusCode if there's an error

--- iss.js
+++ iss.js
@@ -8,16 +8,25 @@ const request = require('request');
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-const url = 'https://api64.ipify.org/?format=json'
+const url = 'https://api64.ipify.org/?format=json';
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
   request(url, (error, response, body) => {
     if (error) {
-      callback(error);
-    } else {
-      callback(null, body);
+      return callback(error, null);
     }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    // in the callback fn it takes in (error, ipAddress)
+    // therefore we have to put (null, ipAddress) meaning at this point, error should be null
+    const ipAddress = JSON.parse(body);
+    callback(null, ipAddress);
+    
   });
 };
 
